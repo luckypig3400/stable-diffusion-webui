9e3bc132dba96a59bacf18faf92474958315c031
郵差 <twHenryYang@gmail.com>
2023-07-21 16:39:47 +0800

    feat: 啟動IP binding設定

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,11 @@
 @echo off
 
+REM https://github.com/AUTOMATIC1111/stable-diffusion-webui/wiki/Command-Line-Arguments-and-Settings#webui-user
+
 set PYTHON="C:\Program Files\Python310\python.exe"
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--listen
+REM https://github.com/AUTOMATIC1111/stable-diffusion-webui/wiki/Command-Line-Arguments-and-Settings#running-within-local-area-network
 
 call webui.bat
