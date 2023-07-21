40d9e277e72461c2c7c6948e0d69e90cb57d7d24
郵差 <twHenryYang@gmail.com>
2023-07-21 16:41:16 +0800

    doc: note for setting sever domain name

--- webui-user.bat
+++ webui-user.bat
@@ -6,6 +6,7 @@ set PYTHON="C:\Program Files\Python310\python.exe"
 set GIT=
 set VENV_DIR=
 set COMMANDLINE_ARGS=--listen
+REM 如果要指定domain name 可以使用此參數 --server-name=stable-diffusion.luckypig.net 但是會和--listen相衝突
 REM https://github.com/AUTOMATIC1111/stable-diffusion-webui/wiki/Command-Line-Arguments-and-Settings#running-within-local-area-network
 
 call webui.bat
